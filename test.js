2b0449e4fbb57e2a1c9473874e9b6f205eae6dfe
Matt <77928207+mattzcarey@users.noreply.github.com>
2022-12-02 11:51:14 +0000

    remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-let array = ["0", "1", "2", "", "3", "4"];
-
-//split array at empty string
-//output = [ [ '0', '1', '2' ], [ '3', '4' ] ]
-const splitArr = (arr) => {
-    
-};
-
-console.log(splitArr(array));
